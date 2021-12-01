356914493f94799fc08a2b29f7a563edaaa2a48a
Anders Bo Rasmussen <anras573@gmail.com>
2021-12-01 10:11:52 +0100

    Finish AOC Day 01

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -4,15 +4,57 @@ namespace AdventOfCode
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
+        private readonly string[] _input;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllLines(InputFilePath);
         }
 
-        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {CalculateIncreasedDepthCount()}");
 
-        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {CalculateThreeMeasurementSlidingWindowDepthCount()}");
+
+        private int CalculateIncreasedDepthCount()
+        {
+            var inputAsInt = _input.Select(i => int.Parse(i));
+
+            int? earlierValue = null;
+            var depthIncreasedCounter = 0;
+
+            foreach (var value in inputAsInt)
+            {
+                if (earlierValue.HasValue && earlierValue.Value < value)
+                {
+                    depthIncreasedCounter++;
+                }
+
+                earlierValue = value;
+            }
+
+            return depthIncreasedCounter;
+        }
+
+        private int CalculateThreeMeasurementSlidingWindowDepthCount()
+        {
+            var inputAsInt = _input.Select(i => int.Parse(i)).ToList();
+
+            int? earlierValue = null;
+            var threeMeasurementSlidingWindowDepthCounter = 0;
+
+            for (int i = 0; i < inputAsInt.Count - 2; i++)
+            {
+                var sum = inputAsInt[i] + inputAsInt[i + 1] + inputAsInt[i + 2];
+
+                if (earlierValue.HasValue && earlierValue.Value < sum)
+                {
+                    threeMeasurementSlidingWindowDepthCounter++;
+                }
+
+                earlierValue = sum;
+            }
+
+            return threeMeasurementSlidingWindowDepthCounter;
+        }
     }
 }
